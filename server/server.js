2b7d5c0799a1a99c8e4438b495eeaa21a0ecbf63
zhaoyanglin <zhaoyanglin@msn.com>
2019-02-05 17:03:01 -0600

    now is done

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static('server/public/jquery-salary-calculator'));
+app.use(express.static('server/public/week-challenge'));
 
 app.listen(5000, function () {
     console.log('running on port 5000');
